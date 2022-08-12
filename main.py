1e15b0e10e1046ffa7d7ed4fb7de260bfc8c0221
lhl12321 <111099800+lhl12321@users.noreply.github.com>
2022-08-12 14:58:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "" + city
+  url = "http://wthrcdn.etouch.cn/WeatherApi?city=娄底" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
